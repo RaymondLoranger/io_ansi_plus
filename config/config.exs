4cac11c2fa80a927b1e2fb90708638eba37bffec
RaymondLoranger <Raymond.Loranger@bell.net>
2020-12-12 16:23:10 -0500

    0.1.28

--- config/config.exs
+++ config/config.exs
@@ -1,13 +1,12 @@
 import Config
 
-# To allow mix messages in colors...
-# config :elixir, ansi_enabled: true
-
 # ANSI escapes are typically enabled on all Unix terminals. They are also
 # available on Windows consoles from Windows 10, although it must be
 # explicitly enabled for the current user in the registry by running the
 # following command:
-
+#
 #     reg add HKCU\\Console /v VirtualTerminalLevel /t REG_DWORD /d 1
-
-import_config "persist_colors.exs"
+#
+# Otherwise, to allow mix messages in colors:
+#
+# config :elixir, ansi_enabled: true
