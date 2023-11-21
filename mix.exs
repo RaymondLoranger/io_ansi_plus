5a9a80326451cbbc369e4c17fec2dc49e833abe1
RaymondLoranger <Raymond.Loranger@bell.net>
2023-11-21 16:28:51 -0500

    0.1.55

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule IO.ANSI.Plus.MixProject do
   def project do
     [
       app: :io_ansi_plus,
-      version: "0.1.54",
+      version: "0.1.55",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       name: "IO ANSI Plus",
